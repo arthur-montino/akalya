8da6fd8bb3c1846a46ed5188ca0a0fdc6f29e9fb
Arthur Montino <arthurmontino2009@gmail.com>
2026-04-18 02:21:22 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <h1>
+      Nick, já te disse o quão bom é conversar contigo? Sei lá, não sei
+      explicar, mas é melhor do que conversar com qualquer pessoa rs, talvez
+      seja só apego, ou talvez eu te ame muito 😈
+    </h1>
+  );
+}
+
+export default Home;
